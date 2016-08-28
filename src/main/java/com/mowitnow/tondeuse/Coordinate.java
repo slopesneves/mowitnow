86c74ca9ef95a40466172ff9257e4e0dd5c3d8b2
Stephane LOPES-NEVES <stephane@lopes-neves.fr>
2016-08-28 15:28:30 +0200

    Coordinate

---
+++ src/main/java/com/mowitnow/tondeuse/Coordinate.java
@@ -0,0 +1,19 @@
+package com.mowitnow.tondeuse;
+
+public class Coordinate {
+    private Short x;
+    private Short y;
+
+    public Coordinate (Short x, Short y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Short getX() {
+        return x;
+    }
+
+    public Short getY() {
+        return y;
+    }
+}
